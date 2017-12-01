2f803d8e583b17b3381b02c2d0c6edb410929f9b
Valentin Fedoskin <valentin.fedoskin@gmail.com>
2017-12-01 22:48:23 +0100

    replace manual dto mapping by external library

--- services/authenticator/src/main/java/com/github/slamdev/micro/playground/services/authenticator/business/boundary/AuthenticationController.java
+++ services/authenticator/src/main/java/com/github/slamdev/micro/playground/services/authenticator/business/boundary/AuthenticationController.java
@@ -1,13 +1,14 @@
 package com.github.slamdev.micro.playground.services.authenticator.business.boundary;
 
-import com.github.slamdev.micro.playground.java.lang.Optionals;
 import com.github.slamdev.micro.playground.libs.authentication.client.JwtFactory;
 import com.github.slamdev.micro.playground.services.authenticator.api.AuthenticatorApi;
+import com.github.slamdev.micro.playground.services.authenticator.api.Credential;
 import com.github.slamdev.micro.playground.services.authenticator.business.control.UserRepository;
 import com.github.slamdev.micro.playground.services.authenticator.business.entity.User;
 import com.nimbusds.jwt.SignedJWT;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
@@ -29,28 +30,26 @@ public class AuthenticationController implements AuthenticatorApi {
 
     private final UserRepository userRepository;
 
+    private final ModelMapper mapper = new ModelMapper();
+
     private <T> T handleDuplicateException(Supplier<T> action) {
         return transformException(action, DuplicateKeyException.class,
                 e -> new HttpClientErrorException(CONFLICT, e.getCause().getMessage()));
     }
 
     @Override
-    public com.github.slamdev.micro.playground.services.authenticator.api.User createUser(com.github.slamdev.micro.playground.services.authenticator.api.Credential credential) {
+    public com.github.slamdev.micro.playground.services.authenticator.api.User createUser(Credential credential) {
         User user = User.builder()
                 .email(credential.getEmail())
                 .password(credential.getPassword())
                 .role(USER)
                 .build();
         User savedUser = handleDuplicateException(() -> userRepository.save(user));
-        return com.github.slamdev.micro.playground.services.authenticator.api.User.builder()
-                .id(savedUser.getId())
-                .email(savedUser.getEmail())
-                .role(Optionals.enumeration(com.github.slamdev.micro.playground.services.authenticator.api.User.Role.class, savedUser.getRole().name()).orElse(null))
-                .build();
+        return mapper.map(savedUser, com.github.slamdev.micro.playground.services.authenticator.api.User.class);
     }
 
     @Override
-    public String generateToken(com.github.slamdev.micro.playground.services.authenticator.api.Credential credential) {
+    public String generateToken(Credential credential) {
         return userRepository.findByEmail(credential.getEmail())
                 .filter(user -> user.getPassword().equals(credential.getPassword()))
                 .map(user -> jwtFactory.create(user.getEmail(), singletonList(user.getRole().toString())))
