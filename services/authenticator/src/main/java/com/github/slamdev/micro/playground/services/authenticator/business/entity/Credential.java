52d0def1e839e3762b003cb91bf65234a4d1cbf6
Valentin Fedoskin <valentin.fedoskin@gmail.com>
2017-12-02 19:50:18 +0100

    remove unused data from authenticator

--- services/authenticator/src/main/java/com/github/slamdev/micro/playground/services/authenticator/business/entity/Credential.java
+++
@@ -1,9 +0,0 @@
-package com.github.slamdev.micro.playground.services.authenticator.business.entity;
-
-import lombok.Value;
-
-@Value
-public class Credential {
-    String email;
-    String password;
-}
