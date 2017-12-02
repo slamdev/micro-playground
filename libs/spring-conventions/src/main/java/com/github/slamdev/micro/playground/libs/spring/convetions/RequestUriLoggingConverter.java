c1d1f10f6bd43bcfd3e3762dc30347fb4c7082b7
Valentin Fedoskin <valentin.fedoskin@gmail.com>
2017-12-02 17:09:33 +0100

    log original request uri on error requests

---
+++ libs/spring-conventions/src/main/java/com/github/slamdev/micro/playground/libs/spring/convetions/RequestUriLoggingConverter.java
@@ -0,0 +1,14 @@
+package com.github.slamdev.micro.playground.libs.spring.convetions;
+
+import ch.qos.logback.core.pattern.DynamicConverter;
+import net.rakugakibox.spring.boot.logback.access.undertow.UndertowLogbackAccessEvent;
+
+public class RequestUriLoggingConverter extends DynamicConverter<UndertowLogbackAccessEvent> {
+
+    @Override
+    public String convert(UndertowLogbackAccessEvent event) {
+        Object originalRequestUri = event.getRequest().getAttribute("javax.servlet.error.request_uri");
+        String uri = originalRequestUri == null ? event.getRequestURI() : originalRequestUri.toString();
+        return uri + event.getQueryString();
+    }
+}
