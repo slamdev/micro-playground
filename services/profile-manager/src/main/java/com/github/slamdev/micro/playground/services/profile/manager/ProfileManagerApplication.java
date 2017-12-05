3d1c9cb5055a1a6d527bfad2743abcd08b8c3b5d
Valentin Fedoskin <valentin.fedoskin@gmail.com>
2017-12-05 21:07:17 +0100

    add base for profile manager service

---
+++ services/profile-manager/src/main/java/com/github/slamdev/micro/playground/services/profile/manager/ProfileManagerApplication.java
@@ -0,0 +1,12 @@
+package com.github.slamdev.micro.playground.services.profile.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfileManagerApplication.class, args);
+    }
+}
